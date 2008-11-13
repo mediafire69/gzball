a2b3e2125137b2f23ee0f826812b4d6731357166
garnier.manuel <garnier.manuel@dad3b1ac-a03c-11dd-91bf-632da8b6e10b>
2008-11-13 10:38:17 +0000

    Nettoyage du code.

--- trunk/ImageManager/src/com/android/app/imageManager/FiltreSimple.java
+++ trunk/ImageManager/src/com/android/app/imageManager/FiltreSimple.java
@@ -3,19 +3,28 @@ package com.android.app.imageManager;
 import java.io.File;
 import java.io.FileFilter;
 
+/**
+ * Class implementing FileFilter interface that permit to create a simple file
+ * filter from a string representing an extension (*.jpg, *.mp3, ...)  
+ */
 public class FiltreSimple implements FileFilter {
-	   // extension acceptée par le filtre
+	   
+	 // The extension to filter
+	  private String extension;
 	  
-	   private String extension;
-	   //Constructeur à partir  de l'extension acceptée
+	   /**
+	    * Constructor
+	    */
 	   public FiltreSimple(String extension){
-	      if(extension ==null){
+	      if(extension == null){
 	         throw new NullPointerException("L'extension ne peut être null.");
 	      }
 	     
 	      this.extension = extension;
 	   }
-	   //Implémentation de FileFilter
+	   /**
+	    * FileFilter implementation
+	    */
 	   public boolean accept(File file){
 	      if(file.isDirectory()) { 
 	         return true; 
